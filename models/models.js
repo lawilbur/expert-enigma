2fa92e8f0040c2ef4d0b261301ba9ed125768046
Eric Sanchez <bluemojo82@gmail.com>
2018-04-06 13:30:04 -0400

    removed year to include as part of title to match API

--- models/models.js
+++ models/models.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const gameSchema = new mongoose.Schema({
     title: String,
     cover: String,
-    year: String,
     description: String,
     likes: {
         type: Number,
